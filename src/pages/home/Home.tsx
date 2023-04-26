28702bc781091e01a2cb036f9109fb3b2a38af81
Orange4Broom <orang4broom@gmail.com>
2023-04-26 09:35:11 +0200

    Add data about films

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,6 +9,13 @@ function Home() {
     "/img/films/9.svg",
   ];
 
+  const names = [
+    "Joker",
+    "The dark knight",
+    "Willy's wonderland",
+    "9"
+  ]
+
   const descriptions = [
     "Description for Image 1",
     "Description for Image 2",
@@ -16,13 +23,22 @@ function Home() {
     "Description for Image 4"
   ];
 
+  const ratings = [
+    "4.1/5",
+    "4.3/5",
+    "3.1/5",
+    "3.5/5"
+  ]
+
   return (
     <>
       <Navigation />
       <div>
         <ImageSlider
           images={images}
+          names={names}
           descriptions={descriptions}
+          ratings={ratings}
           autoplayInterval={3000}
         />
       </div>
