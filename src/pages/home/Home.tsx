3b462ef70ada062b990bf29ec6b9a2c58dbd6ccd
Jakub Trnčák <orangebroom@Jakubs-MacBook-Pro.local>
2023-04-26 11:20:41 +0200

    Finish image-slider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -19,10 +19,10 @@ function Home() {
   ]
 
   const descriptions = [
-    "Description for Image 1",
-    "Description for Image 2",
-    "Description for Image 3",
-    "Description for Image 4"
+    "A socially inept clown for hire - Arthur Fleck aspires to be a stand up comedian among his small job working dressed as a clown holding a sign for advertising. He takes care of his mother- Penny Fleck, and as he learns more about his mental illness, he learns more about his past.",
+    "With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos.",
+    "A quiet drifter is tricked into a janitorial job at the now condemned Willy's Wonderland. The mundane tasks suddenly become an all-out fight for survival against wave after wave of demonic animatronics. Fists fly, kicks land, titans clash -- and only one side will make it out alive.",
+    "A rag doll that awakens in a postapocalyptic future holds the key to humanity's salvation."
   ];
 
   const ratings = [
@@ -41,7 +41,7 @@ function Home() {
           names={names}
           descriptions={descriptions}
           ratings={ratings}
-          autoplayInterval={5000}
+          autoplayInterval={6000}
         />
       </div>
     </>
